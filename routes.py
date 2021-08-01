7cbb82c209b300678dac2e82e3f3bdb097db1c9f
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-08-01 18:01:54 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -76,10 +76,10 @@ def result():
 def admin():
     return render_template('admin.html')
 
-@main.errorhandler(404)
+@main.app_errorhandler(404)
 def not_found(e):
     return render_template('404.html'), 404
 
-@main.errorhandler(500)
+@main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
