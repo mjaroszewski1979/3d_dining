9d983344cb0aa58673decc3e312237b447ba1454
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-29 21:59:57 +0100

    Update tests.py

--- tests_selenium/tests.py
+++ tests_selenium/tests.py
@@ -37,9 +37,29 @@ class SeleniumTest(TestBase):
         assert home_page.is_home_link_works()
         assert home_page.is_menu_link_works()
 
+    def test_menu_page(self):
+        self.driver.get('http://127.0.0.1:5000/menu')
+        menu_page = page.MenuPage(self.driver)
+        assert menu_page.is_title_matches()
+        assert menu_page.is_info_displayed_correctly()
+        assert menu_page.is_manage_booking_button_works()
+
+    def test_error_page(self):
+        self.driver.get('http://127.0.0.1:5000/error')
+        error_page = page.ErrorPage(self.driver)
+        assert error_page.is_title_matches()
+        assert error_page.is_error_msg_displayed()
+
+    def test_book_page(self):
+        self.driver.get('http://127.0.0.1:5000/form')
+        book_page = page.BookPage(self.driver)
+        assert book_page.is_title_matches()
+        assert book_page.is_form_works_given_invalid_number_of_guests()
+        assert book_page.is_form_works_given_invalid_date()
+        assert book_page.is_form_works_when_fully_booked()
+        assert book_page.is_form_works_given_valid_data()
+
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-        
