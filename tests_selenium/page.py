c7f413ad7815661f47a8a43e27de2f0e6d4d34cf
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-30 18:35:32 +0100

    Update page.py

--- tests_selenium/page.py
+++ tests_selenium/page.py
@@ -1,9 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait as W
 from selenium.webdriver.support import expected_conditions as EC
-from locators import BookPageLocators, ErrorPageLocators, HomePageLocators, MenuPageLocators
+from locators import BookPageLocators, ErrorPageLocators, HomePageLocators, MenuPageLocators, AdminPageLocators
 from selenium.webdriver.common.action_chains import ActionChains
-
-
 
 
 class BasePage(object):
@@ -33,7 +31,19 @@ class BasePage(object):
         element = W(self.driver, 10).until(EC.visibility_of_element_located(locator))
         return element.text
 
-    
+    def send_booking(self, date, guests, email):
+        self.do_clear(BookPageLocators.BOOK_DATE)
+        self.do_clear(BookPageLocators.GUESTS)
+        self.do_clear(BookPageLocators.EMAIL)
+        self.do_send_keys(BookPageLocators.BOOK_DATE, date)
+        self.do_send_keys(BookPageLocators.GUESTS, guests)
+        self.do_send_keys(BookPageLocators.EMAIL, email)
+        self.do_click(BookPageLocators.SUBMIT_BOOK)
+
+    def fill_admin_form(self, date):
+        self.do_clear(AdminPageLocators.DATE)
+        self.do_send_keys(AdminPageLocators.DATE, date)
+        self.do_click(AdminPageLocators.SUBMIT)
 
 
 class HomePage(BasePage):
@@ -75,31 +85,39 @@ class BookPage(BasePage):
         return "3 Dining | Book" in self.driver.title
 
     def is_form_works_given_valid_data(self):
-        self.do_send_keys(BookPageLocators.DATE, '11/10/2022')
-        self.do_send_keys(BookPageLocators.GUESTS, 2)
-        self.do_send_keys(BookPageLocators.EMAIL, 'mj@gmail.com')
-        self.do_click(BookPageLocators.SUBMIT)
+        self.send_booking('11/10/2022', 5, 'mj@gmail.com')
         return "3 Dining | Success" in self.driver.title
 
     def is_form_works_given_invalid_number_of_guests(self):
-        self.do_send_keys(BookPageLocators.DATE, '11/10/2022')
-        self.do_send_keys(BookPageLocators.GUESTS, 222)
-        self.do_send_keys(BookPageLocators.EMAIL, 'mj@gmail.com')
-        self.do_click(BookPageLocators.SUBMIT)
+        self.send_booking('11/10/2022', 555, 'mj@gmail.com')
         error_msg = self.get_element_text(BookPageLocators.ERROR_MSG)
         return "Sorry, maximum number of guests per day is 30." in error_msg
 
     def is_form_works_given_invalid_date(self):
-        self.do_clear(BookPageLocators.DATE)
-        self.do_clear(BookPageLocators.GUESTS)
-        self.do_clear(BookPageLocators.EMAIL)
-        self.do_send_keys(BookPageLocators.DATE, '20/11/2022')
-        self.do_send_keys(BookPageLocators.GUESTS, 2)
-        self.do_send_keys(BookPageLocators.EMAIL, 'mj@gmail.com')
-        self.do_click(BookPageLocators.SUBMIT)
+        self.send_booking('20/11/2022', 5, 'mj@gmail.com')
         error_msg = self.get_element_text(BookPageLocators.ERROR_MSG)
         return "Sorry, we are closed on weekends. Please choose a different date." in error_msg
 
+    def is_form_works_when_fully_booked(self):
+        self.do_click(HomePageLocators.FORM_LINK)
+        self.send_booking('11/10/2022', 29, 'mj@gmail.com')
+        error_msg = self.get_element_text(BookPageLocators.ERROR)
+        return "Sorry, we are already fully booked. Please choose a different date." in error_msg
+
+class AdminPage(BasePage):
+
+    def is_title_matches(self):
+        return "3 Dining | Admin" in self.driver.title
+
+    def is_form_works_given_invalid_date(self):
+        self.fill_admin_form('18/11/2022')
+        error_msg = self.get_element_text(AdminPageLocators.FORM_ERROR)
+        return 'There are no bookings on the date you have selected!' in error_msg
+
+    def is_form_works_given_valid_date(self, email):
+        self.fill_admin_form('11/10/2022')
+        email_text = self.get_element_text(AdminPageLocators.EMAILS)
+        return email in email_text
 
 class ErrorPage(BasePage):
 
@@ -113,5 +131,4 @@ class ErrorPage(BasePage):
 
 
 
-   
    
